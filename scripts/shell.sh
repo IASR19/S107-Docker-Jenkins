e1fc6351e8ee622223ed838b00cfcf4b11f85826
iasr19 <itamar.asr19@gmail.com>
2023-05-29 19:46:10 -0300

    :recycle: Change itens

---
+++ scripts/shell.sh
@@ -0,0 +1,7 @@
+ls
+cd S107-Docker-Jenkins
+ls
+echo "HAHAHAHAHAH"
+sudo apt install mailutils
+echo "fim da instalacao"
+echo "Mandando e-mail com mail do linux" | mail -s "meu email deu tudo certo" chris.souzalima@outlook.com
